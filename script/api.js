0316c95ca3a8cc764500ab976c67436792b287d6
leila <mbiydzenyuyeileen@gmail.com>
2025-01-15 13:50:26 +0100

    adding the js logic to project

--- script/api.js
+++ script/api.js
@@ -1,14 +1,14 @@
-const API_KEY = "your_api_key_here";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = '4ef363f9f9a3c5535149c90970fa2311'
+const BASE_URL = 'https://api.themoviedb.org/3'
 
 async function fetchMovies(endpoint) {
-  const response = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}`);
-  return response.json();
+  const response = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}`)
+  return response.json()
 }
 
 async function searchMovies(query) {
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
-  );
-  return response.json();
+  )
+  return response.json()
 }
---
+++ script/movieList.js
@@ -0,0 +1,20 @@
+function displayMovies(movies) {
+  const movieList = document.getElementById('movie-list')
+  movieList.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const movieElement = createMovieElement(movie)
+    movieList.appendChild(movieElement)
+  })
+}
+
+function createMovieElement(movie) {
+  const movieElement = document.createElement('div')
+  movieElement.classList.add('movie')
+  movieElement.innerHTML = `
+        <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}' alt='${movie.title}'>
+        <h2>${movie.title}</h2>
+    `
+  movieElement.addEventListener('click', () => showMovieDetails(movie.id))
+  return movieElement
+}
