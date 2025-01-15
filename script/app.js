9bb010900607fbaf3c099d0fcf49ab55e875f1d1
leila <mbiydzenyuyeileen@gmail.com>
2025-01-15 13:57:07 +0100

    adding the js logic to project

---
+++ script/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const popularMovies = await fetchMovies('/movie/popular')
+  displayMovies(popularMovies.results)
+
+  const searchBar = document.getElementById("searchBar")
+  searchBar.addEventListener('input', async (e) => {
+    if (e.target.value) {
+      const searchResults = await searchMovies(e.target.value)
+      displayMovies(searchResults.results)
+    } else {
+      const popularMovies = await fetchMovies('/movie/popular')
+      displayMovies(popularMovies.results)
+    }
+  })
+
+  const showFavoritesButton = document.getElementById('show-favorites')
+  showFavoritesButton.addEventListener('click', displayFavorites)
+})
